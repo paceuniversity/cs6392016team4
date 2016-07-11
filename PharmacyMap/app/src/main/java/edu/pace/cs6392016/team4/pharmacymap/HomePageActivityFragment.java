87847ae8189b1042a819c90e4d22b74e8f9f2411
Remille <sophia@yahoo.com>
2016-07-11 19:34:16 -0400

    took map code and execute it on button click, added test apk

--- PharmacyMap/app/src/main/java/edu/pace/cs6392016/team4/pharmacymap/HomePageActivityFragment.java
+++ PharmacyMap/app/src/main/java/edu/pace/cs6392016/team4/pharmacymap/HomePageActivityFragment.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -19,14 +21,29 @@ public class HomePageActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.home_page_fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.home_page_fragment_main, container, false);
+        final EditText zipCode = (EditText) rootView.findViewById(R.id.searchMap);
+        Button search = (Button)rootView.findViewById(R.id.button);
+        search.setOnClickListener( new View.OnClickListener() {
+           @Override
+            public void onClick(View v) {
+               String input;
+               if (zipCode.getText().length() > 0) {
+                   input = zipCode.getText().toString();
+                   input = "geo:?q=pharmacies " + input;
+               }
+               else {
+                   input = "geo:40.710412,-74.006137?q=pharmacies";
+               }
+               Uri gmmIntentUri = Uri.parse(input);
+               Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+               mapIntent.setPackage("com.google.android.apps.maps");
+               startActivity(mapIntent);
+           }
+        });
 
-        Uri gmmIntentUri = Uri.parse("geo:40.710412,-74.006137?q=pharmacies");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");
-        startActivity(mapIntent);
 
-        return v;
+        return rootView;
 
     }
 }
