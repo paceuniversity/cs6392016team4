02ac4722aeabeb6cb4baa516c60212951d7e98e8
Rahul Sood <rahulsood@Rahuls-MacBook-Air.local>
2016-07-10 23:28:44 -0400

    added map at homepage

--- PharmacyMap/app/src/main/java/edu/pace/cs6392016/team4/pharmacymap/GoogleMarker.java
+++
@@ -1,7 +0,0 @@
-package edu.pace.cs6392016.team4.pharmacymap;
-
-/**
- * Created by rahulsood on 7/8/16.
- */
-public class GoogleMarker {
-}
--- PharmacyMap/app/src/main/java/edu/pace/cs6392016/team4/pharmacymap/HomePageActivityFragment.java
+++ PharmacyMap/app/src/main/java/edu/pace/cs6392016/team4/pharmacymap/HomePageActivityFragment.java
@@ -7,12 +7,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-
-import org.apache.commons.csv.CSVRecord;
-
-import java.util.concurrent.ExecutionException;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -25,41 +19,14 @@ public class HomePageActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.home_page_fragment_main,container,false);
-        final EditText zipCode = (EditText) rootView.findViewById(R.id.searchMap);
-        Button lookUp = (Button)rootView.findViewById(R.id.executeQuery);
-        lookUp.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String input;
-                if(zipCode.getText().length() > 0) {
-                    input = "10038";
-                }
-                else {
-                    input = zipCode.getText().toString();
-                }
+        View v = inflater.inflate(R.layout.home_page_fragment_main, container, false);
 
-                MapMarkerAsyncTask mat = new MapMarkerAsyncTask();
-                CSVRecord pharmacy = null;
-                mat.execute(input);
-                try {
-                    pharmacy = mat.get();
-                    String query = pharmacy.get("FacilityBuildingNo") +
-                            " " +
-                            pharmacy.get("FacilityStreetName");
-                    String geoUri = "geo:"+query+"?z=12";
-                    Uri geo = Uri.parse(geoUri);
-                    Intent intent = new Intent(Intent.ACTION_VIEW,geo);
-                    startActivity(intent);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                }
+        Uri gmmIntentUri = Uri.parse("geo:40.710412,-74.006137?q=pharmacies");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
 
+        return v;
 
-            }
-        });
-        return rootView;
     }
 }
